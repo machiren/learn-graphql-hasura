700215ac96f931cfe004ac494ddad88bdf1cc364
ren machida <r-machida@c3reve.co.jp>
2021-01-24 22:35:04 +0900

    #17 見た目を整える2

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from "next/dist/client/router";
 import { NextPage } from 'next';
 import Error from 'next/error';
+import Image from 'next/image';
 import { useGetArticleQuery } from '../../../generated/graphql';
+import styles from './index.module.css';
 
 const ArticlePage: NextPage = () => {
   const router = useRouter();
@@ -22,10 +24,18 @@ const ArticlePage: NextPage = () => {
   }
   const { user, subject, content } = data.articles_by_pk;
   return  (
-    <div>
-      <div>{JSON.stringify(user)}</div>
-      <div>{subject}</div>
-      <div>{content}</div>
+    <div className={styles.contentContainer}>
+      <h1 className={styles.subject}>{subject}</h1>
+      <div className={styles.userContainer}>
+        <div>
+          <Image className={styles.userIcon} src="/public/profile.jpg" width={48} height={48} />
+        </div>
+        <div>
+          <div className={styles.userId}>{user.displayId}</div>
+          <span className={styles.userName}>{user.displayName}</span>
+        </div>
+      </div>
+      <div className={styles.content}>{content}</div>
     </div>
   )
 }
